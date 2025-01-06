4c089ccb343d8633470c784d5d8d071ba2140a0c
sarah cristina <sarahsilva34567@gmail.com>
2025-01-06 20:53:37 -0300

    cria  projeto vivo-morto com react 19

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Emoji from './Emoji';
+
+function App() {
+  
+  return (
+    <>
+     <Emoji/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Emoji.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "./Emoji.css";
+
+const EMOJIS = new Map <string,string>(
+    [
+        ["happy", "🙂"],
+        ["sick", "🤢"],
+        ["dead", "😵"],
+        ["sad", "😞"]
+    ]
+);
+
+export default function Emoji(){
+     const[situacao, setSituacao] = useState("happy");
+
+    function toHappy(){
+        console.log(`toHappy()!" ${situacao}`);
+        setSituacao("happy");
+        console.log(`toHappy()!" ${situacao}`);
+    }
+
+    function toDead(){
+        console.log("toDead()!");
+        setSituacao("dead");
+    }
+
+    function toSick(){
+        console.log("toSick()!");
+        setSituacao("sick");
+    }
+
+    function toSad(){
+        console.log("toSick()!");
+        setSituacao("sad");
+    }
+
+    function cicle(){
+        console.log("cicle()!");
+        switch(situacao){
+            case "happy":
+                setSituacao("sick");
+                break;
+            case "sick":
+                setSituacao("dead");
+                break;
+            case "dead":
+                setSituacao("sad")
+                break;
+            case "sad":
+                setSituacao("happy");
+                break;
+            default:
+                setSituacao("happy");
+        }
+    }
+    return(
+        <div className="emoji">
+        <div className="situacao">{EMOJIS.get(situacao) || "🫥"}</div>
+        <div className="acoes">
+            <button onClick={toHappy}>Vivo</button>
+            <button onClick={toSick}>Sick</button>
+            <button onClick={toDead}>morto</button>
+            <button onClick={toSad}>Sad</button>
+            <button onClick={cicle}>cicle</button>
+        </div>
+        </div>
+    )       
+}
